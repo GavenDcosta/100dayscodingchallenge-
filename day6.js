5f3baa733655808d980daf416769b6c650a0a871
Gaven Dcosta <112816730+TeddyGaven@users.noreply.github.com>
2022-09-19 21:02:02 +0530

    Add files via upload

---
+++ day6.js
@@ -0,0 +1,54 @@
+a = document.getElementById('txt1')
+add = document.getElementById('btn1');
+list = document.getElementById('container');
+save = document.getElementById('btn2')
+
+
+
+add.addEventListener('click', function(){ 
+    task = a.value
+    localStorage.setItem('Name',task)
+    a.value = ""
+})
+
+save.addEventListener('click', function(){
+    var para = document.createElement('p')
+    para.classList.add('para')
+    para.textContent = JSON.stringify(localStorage.getItem("Name"))
+    list = list.appendChild(para)
+
+    para.addEventListener('click', function(){
+        para.style.textDecoration = 'line-through';
+
+        para.addEventListener('click', function(){
+            para.style.textDecoration = ""
+
+            para.addEventListener('dblclick', function(){
+                list.removeChild(para);     
+             })
+           
+        })
+    })
+  })
+
+
+    //para.addEventListener('click', function(){If(para.style.textDecoration = "")
+   // {
+    // para.style.textDecoration = 'line-through'
+    //}
+    //Else 
+    //{
+    // para.style.textDecoration = ""
+   // }
+   // }) 
+
+   
+
+
+  
+
+
+
+
+
+
