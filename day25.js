1339046f52f828f24b7d7dd722cd81f478f26c86
Gaven Dcosta <112816730+TeddyGaven@users.noreply.github.com>
2022-10-08 10:32:44 +0530

    Add files via upload

---
+++ day25.js
@@ -0,0 +1,8 @@
+a = document.getElementById('txt1')
+b = document.getElementById('btn1')
+c = document.getElementById('txt2')
+
+b.addEventListener('click', ()=>{
+      let d = parseFloat(a.value) * 2.2
+      c.innerHTML = d + '  lbs' 
+})
