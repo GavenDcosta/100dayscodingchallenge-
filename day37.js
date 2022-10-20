a0431e7a501f4b3276dd721291ee9af9709f2937
Gaven Dcosta <112816730+TeddyGaven@users.noreply.github.com>
2022-10-20 13:08:50 +0530

    Add files via upload

---
+++ day37.js
@@ -0,0 +1,21 @@
+a = document.getElementById('txt1')
+b = document.getElementById('txt2')
+c = document.getElementById('txt3')
+d = document.getElementById('btn1')
+
+d.addEventListener('click', ()=>{
+    let n = b.value
+    let m = a.value
+    c.innerHTML = parseFloat(m) * parseFloat(n) 
+
+    if(parseFloat(c.value)>=50){
+        document.body.style.backgroundColor = "red"
+    }
+    else if(parseFloat(c.value)<=50){
+        document.body.style.backgroundColor = "green"
+    }
+
+
+    
+})
+
