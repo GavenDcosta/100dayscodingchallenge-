1f731375d813ecdf37c30233fbc6a12b3cead62c
Gaven Dcosta <112816730+TeddyGaven@users.noreply.github.com>
2022-10-24 12:53:02 +0530

    Add files via upload

---
+++ day41.js
@@ -0,0 +1,12 @@
+const person = {
+    name: 'gaven',
+    walk() {},
+    talk() {}
+};
+
+person.talk();
+person.name = ''
+
+const targetValue = 'name'
+person[targetValue.value] = 'luffy'
+console.log(person)
