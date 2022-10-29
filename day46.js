0885d6d295ea66958077c01c916d09e7dd8fbf08
Gaven Dcosta <112816730+TeddyGaven@users.noreply.github.com>
2022-10-29 12:11:11 +0530

    Add files via upload

---
+++ day46.js
@@ -0,0 +1,16 @@
+const react = (
+<div>
+
+<h1>My top 5 best anime</h1>
+<ul>
+    <li>One Piece</li>
+    <li>Naruto</li>
+    <li>Kingdom</li>
+    <li>Piano no mori</li>
+    <li>Reincarnated as a slime</li>
+</ul>
+
+</div>
+)
+//there are 2 ways of writing img tag ...ive written both on line 27 & 28
+ReactDOM.createRoot(document.getElementById('root')).render(react)
