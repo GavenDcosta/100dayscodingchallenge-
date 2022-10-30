c54a71e03a77e95ab980a5ab52afc9db217adce3
Gaven Dcosta <112816730+TeddyGaven@users.noreply.github.com>
2022-10-30 17:15:53 +0530

    Add files via upload

---
+++ day47.js
@@ -0,0 +1,14 @@
+const page = (
+   
+     <h1>It is a shame for a man to become old without watching the beauty and the beast inside of him  ~ C BUM</h1>
+    
+)
+
+const root = (
+    
+        <h1>Do the hard work especially when you dont feel like it  ~Hamza Ahemad</h1>
+  
+)
+
+ReactDOM.createRoot(document.getElementById('root')).render(page)
+ReactDOM.createRoot(document.getElementById('fruit')).render(root)
