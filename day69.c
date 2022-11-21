afbef8cee2f7afd73f54d52f96576d39647fc5bd
Gaven Dcosta <112816730+TeddyGaven@users.noreply.github.com>
2022-11-21 20:06:41 +0530

    Add files via upload

---
+++ day69.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+    int a;
+    printf("Enter your birth year");
+    scanf("%d",&a);
+    int sum = 2022-a;
+    printf("Your age is %d",sum); 
+    return 0;    
+}
