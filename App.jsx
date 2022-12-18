2e8ea3760a7acfb8b9e6ffbdbd4b07b017e5584a
Gaven Dcosta <112816730+TeddyGaven@users.noreply.github.com>
2022-12-18 16:03:38 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,32 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+
+function Music({img,composer,songName,mp3}){
+  return(
+      <div className="main">
+          <img src={img}/>
+          <div id="Name">
+          <h3>Composer - {composer}</h3>
+          <h4>Name - {songName}</h4>
+          <audio src={mp3} loop controls></audio>
+          </div>
+      </div>
+  )
+}
+
+function App() {
+  return (
+    <div className="head">
+    <Music 
+       img="../images/92keys.jpg" 
+       composer="92 Keys"
+       songName="Uprising on Bach's Toccata"
+       mp3="../mp3/bachtoccata.mp3"
+    />
+</div>
+  )
+}
+
+export default App
