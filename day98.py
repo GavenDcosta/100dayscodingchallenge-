8f065733cbe804612795edd98a457d21056a6e6e
Gaven Dcosta <112816730+TeddyGaven@users.noreply.github.com>
2022-12-21 13:44:59 +0530

    Add files via upload

---
+++ day98.py
@@ -0,0 +1,22 @@
+x = int(input("Enter a no from 1-7"))
+
+match x:
+    
+    case 1:
+        print("Sunday")
+    case 2:
+        print("Monday")
+    case 3:
+        print("Tuesday")
+    case 4:
+        print("Wednesday")
+    case 5:
+        print("Thursday")
+    case 6:
+        print("Friday")
+    case 7:
+        print("Saturday") 
+    case _:
+        print("Wrong Input")    
+        
+                                 
